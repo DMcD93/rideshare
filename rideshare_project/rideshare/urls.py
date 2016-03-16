e0b36b7e3e1ad5544b32485557b8a08035f0ef85
DMcD93 <DMcD93@hotmail.co.uk>
2016-03-16 14:50:19 +0000

    Log-in page working

--- rideshare_project/rideshare/urls.py
+++ rideshare_project/rideshare/urls.py
@@ -3,8 +3,9 @@ from rideshare import views
 
 urlpatterns = patterns('',
     url(r'^$', views.main, name='main'),
-	url(r'^login', views.login, name='login'),
+	url(r'^logout/$', views.user_logout, name='logout'),
 	url(r'^register', views.register, name='register'),
+	url(r'^login/$', views.user_login, name='login'),
 	url(r'^post_ride', views.post_ride, name='post_ride'),
 	#url(r'^search_ride', views.search_ride, name='search_ride'),
 )
--- rideshare_project/rideshare/views.py
+++ rideshare_project/rideshare/views.py
@@ -1,17 +1,66 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from rideshare.forms import UserForm, UserRegForm
 #from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from rideshare.models import Journey
 
 def main(request):
 	return render(request, 'rideshare/main.html')	
 #    return HttpResponse("This is the main page")
 
-def login(request):
-	return render(request, 'rideshare/login.html')	
+def user_login(request):
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+                # We use request.POST.get('<variable>') as opposed to request.POST['<variable>'],
+                # because the request.POST.get('<variable>') returns None, if the value does not exist,
+                # while the request.POST['<variable>'] will raise key error exception
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/rideshare/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your rideshare account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'rideshare/login.html', {})
+		
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/rideshare/')
 
 def register(request):
 	registered = False
