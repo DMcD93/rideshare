88d0e285704f342a903dc9e5932ad9db903d6cc8
=DMcD93 <dmcd93@hotmail.co.uk>
2016-03-25 15:14:45 +0000

    Population scripts working

--- rideshare_project/populate_vehicle.py
+++ rideshare_project/populate_vehicle.py
@@ -8,25 +8,19 @@ from django.utils import timezone
 from rideshare.models import Vehicle
 from django.contrib.auth.models import User
 
-    reg_no = models.CharField(max_length=8)
-    make = models.CharField(max_length=30)
-    model = models.CharField(max_length=30)
-    user = models.ForeignKey(User, primary_key=True)
-
 def populate():
 	add_vehicle(user='Harry',
 	reg_no='SJ01 5PU',
 	make='Ford',
 	model='Escort')
-
 	
 	add_vehicle(user='leifos',
 	reg_no='LE1 7OS',
 	make='Aston Martin',
 	model='DB9')
 	
-def add_add_vehicle(user, reg_no, make, model):
-	a = Review.objects.get_or_create(user=user)[0]
+def add_vehicle(user, reg_no, make, model):
+	a = Vehicle.objects.get_or_create(user__username=user)[0]
 	a.reg_no=reg_no
 	a.make=make
 	a.model=model
